f4382709f4e303cfca3e335344d1313736fa3585
caiobinharj <caioasc886@gmail.com>
2023-11-30 22:19:09 -0300

    feat: imagem de vitoria

--- main.py
+++ main.py
@@ -13,51 +13,71 @@ pygame.display.set_caption("Rio Breakdown")
 #fases: prisao (1), refeitorio (2), cozinha (3), quadra (4), estacionamento (5), lixão (6), pátio (7) e lago (8)
 fase_atual = 1
 
+#função colisão
+
+
 #itens
 pedecabra_visivel = True
 pedecabra = pygame.image.load("items/pedecabra.png")
 pedecabra = pygame.transform.scale(pedecabra, (50, 50))
-posicao_pedecabra = (750, 350)
+posicao_pedecabra = (700, 350)
+colisao_pedecabra = False
 
 botas_visivel = True
 botas = pygame.image.load("items/botasmagicas.png")
 botas = pygame.transform.scale(botas, (50, 50))
 posicao_botas = (12, 130)
+colisao_botas = False
+
 
 panela_visivel = True
 panela = pygame.image.load("items/panela.png")
 panela = pygame.transform.scale(panela, (50, 50))
 posicao_panela = (110, 80)
+colisao_panela = False
+
 
 peixe_visivel = True
 peixe = pygame.image.load("items/peixao.png")
 peixe = pygame.transform.scale(peixe, (50, 50))
 posicao_peixe = (700, 500)
+colisao_peixe = False
+
 
 apito_visivel = True
 apito = pygame.image.load("items/apito.png")
 apito = pygame.transform.scale(apito, (50, 50))
 posicao_apito = (365, 105)
+colisao_apito = False
+
 
 pirotecnia_visivel = True
 pirotecnia = pygame.image.load("items/pirotecnia.png")
 pirotecnia = pygame.transform.scale(pirotecnia, (24, 30))
 posicao_pirotecnia = (25, 500)
+colisao_pirotecnia = False
+
 
 geocinese_visivel = True
 geocinese = pygame.image.load("items/geocinese.png")
 geocinese = pygame.transform.scale(geocinese, (24, 30))
 posicao_geocinese = (300, 100)
+colisao_geocinese = False
+
 
 tormenta_visivel = True
 tormenta = pygame.image.load("items/tormenta.png")
 tormenta = pygame.transform.scale(tormenta, (24, 30))
 posicao_tormenta = (588, 555)
+colisao_tormenta = False
+
 
 vortex_visivel = True
 vortex = pygame.image.load("items/vortex.png")
 vortex = pygame.transform.scale(vortex, (24, 30))
 posicao_vortex = (522, 60)
+colisao_vortex = False
+
 
 
 #littlemor
@@ -126,8 +146,8 @@ sab = pygame.transform.scale(sab, (novo_largura, nova_altura))
 sab_revertido = pygame.transform.flip(sab, True, False)
 posicao_sab = (650, 475)
 dialogo_aberto_sab = False
-texto_sab1 = ("Brother, bizu: busca umas carta por aí ta ligado,")
-texto_sab2 = ("tu consegue mlk bom.")
+texto_sab1 = ("Traga-me as 4 cartas escondidas no mapa meu nobre.")
+texto_sab2 = ("Faça isso e Nós dá fuga paizin")
 
 #slots
 slot_inventario = pygame.image.load("slot.png")
@@ -143,7 +163,8 @@ posicao_slot7 = (largura // 2 - 150, altura-100)
 posicao_slot8 = (largura // 2 - 50, altura-100 )
 posicao_slot9 = (largura // 2 + 50, altura-100 )
 posicao_slot10 = (largura // 2 + 150, altura-100 )
-
+slots = []
+posicao_item_slot = [(largura//2  - 220, 30),(largura//2  - 120, 30),(largura//2  - 20, 30),(largura//2 + 80, 30),(largura//2  + 180, 30),(largura//2  - 220, altura - 75),(largura//2  - 120, altura-75),(largura//2  - 20, altura -75),(largura//2  +80, altura-75),(largura//2  +180, altura-75)]
 
 
 slots_visiveis = False
@@ -159,23 +180,42 @@ while executando:
             if evento.key == pygame.K_s:
                 slots_visiveis = not slots_visiveis  # Alternar a visibilidade dos slots
 
-            if evento.key == pygame.K_SPACE and fase_atual==5:
+            if evento.key == pygame.K_SPACE and colisao_pedecabra:
                 pedecabra_visivel = False
-            if evento.key == pygame.K_SPACE and fase_atual==6:
+                slots.append(pedecabra)
+
+            if evento.key == pygame.K_SPACE and colisao_botas:
                 botas_visivel = False
-            if evento.key == pygame.K_SPACE and fase_atual==3:
+                slots.append(botas)
+
+            if evento.key == pygame.K_SPACE and colisao_panela:
                 panela_visivel = False
-            if evento.key == pygame.K_SPACE and fase_atual==8:
+                slots.append(panela)
+
+            if evento.key == pygame.K_SPACE and colisao_peixe:
                 peixe_visivel = False
+                slots.append(peixe)
+
+            if evento.key == pygame.K_SPACE and colisao_vortex:
                 vortex_visivel = False
-            if evento.key == pygame.K_SPACE and fase_atual==7:
+                slots.append(vortex)
+
+            if evento.key == pygame.K_SPACE and colisao_apito:
                 apito_visivel = False
-            if evento.key == pygame.K_SPACE and fase_atual==2:
+                slots.append(apito)
+
+            if evento.key == pygame.K_SPACE and colisao_pirotecnia:
                 pirotecnia_visivel = False
-            if evento.key == pygame.K_SPACE and fase_atual==1:
+                slots.append(pirotecnia)
+
+            if evento.key == pygame.K_SPACE and colisao_geocinese:
                 geocinese_visivel = False
-            if evento.key == pygame.K_SPACE and fase_atual==4:
+                slots.append(geocinese)
+
+            if evento.key == pygame.K_SPACE and colisao_tormenta:
                 tormenta_visivel = False
+                slots.append(tormenta)
+
 
 
     teclas = pygame.key.get_pressed()
@@ -274,9 +314,42 @@ while executando:
 
     camera.center = retangulo_personagem.center
 
+
+    retangulo_pirotecnia = pygame.Rect(posicao_pirotecnia[0], posicao_pirotecnia[1], 20, 20)
+    if fase_atual==2:
+        colisao_pirotecnia = retangulo_personagem.colliderect((retangulo_pirotecnia))
+
+    retangulo_panela = pygame.Rect(posicao_panela[0], posicao_panela[1], 30, 30)
+    if fase_atual == 3:
+        colisao_panela = retangulo_personagem.colliderect((retangulo_panela))
+
+    retangulo_tormenta = pygame.Rect(posicao_tormenta[0], posicao_tormenta[1], 20, 20)
+    if fase_atual == 4:
+        colisao_tormenta = retangulo_personagem.colliderect((retangulo_tormenta))
+
+    retangulo_pedecabra = pygame.Rect(posicao_pedecabra[0], posicao_pedecabra[1], 30, 30)
+    if fase_atual == 5:
+        colisao_pedecabra = retangulo_personagem.colliderect((retangulo_pedecabra))
+
+    retangulo_botas = pygame.Rect(posicao_botas[0], posicao_botas[1], 30, 30)
+    if fase_atual == 6:
+        colisao_botas = retangulo_personagem.colliderect((retangulo_botas))
+
+    retangulo_apito = pygame.Rect(posicao_apito[0], posicao_apito[1], 30, 30)
+    if fase_atual == 7:
+        colisao_apito = retangulo_personagem.colliderect((retangulo_apito))
+
+    retangulo_vortex = pygame.Rect(posicao_vortex[0], posicao_vortex[1], 20, 20)
+    retangulo_peixe = pygame.Rect(posicao_peixe[0], posicao_peixe[1], 30, 30)
+    if fase_atual == 8:
+        colisao_vortex = retangulo_personagem.colliderect((retangulo_vortex))
+        colisao_peixe = retangulo_personagem.colliderect(retangulo_peixe)
+
+
     if fase_atual ==1:
         background = pygame.image.load("prisao.png")
         background = pygame.transform.scale(background, (largura, altura))
+
 
 
         retangulo_blate = pygame.Rect(posicao_blate[0], posicao_blate[1], 50, 50)
@@ -299,6 +372,42 @@ while executando:
         colisao_sab = retangulo_personagem.colliderect(retangulo_sab)
         distancia_entre_personagem_e_sab = abs(posicao_x - posicao_sab[0])
 
+        retangulo_geocinese = pygame.Rect(posicao_geocinese[0], posicao_geocinese[1], 20, 20)
+        colisao_geocinese = retangulo_personagem.colliderect((retangulo_geocinese))
+
+
+
+    if fase_atual == 2:
+        background = pygame.image.load("refeitorio.png")
+        background = pygame.transform.scale(background, (largura, altura))
+
+
+    if fase_atual==3:
+        background = pygame.image.load("cozinha.png")
+        background = pygame.transform.scale(background, (largura, altura))
+
+    if fase_atual==4:
+        background = pygame.image.load("quadra.png")
+        background = pygame.transform.scale(background, (largura, altura))
+
+
+    if fase_atual == 5:
+        background = pygame.image.load("estacionamento.png")
+        background = pygame.transform.scale(background, (largura, altura))
+
+    if fase_atual == 6:
+        background = pygame.image.load("lixao.png")
+        background = pygame.transform.scale(background, (largura, altura))
+
+    if fase_atual == 7:
+        background = pygame.image.load("patio.png")
+        background = pygame.transform.scale(background, (largura, altura))
+
+    if fase_atual == 8:
+        background = pygame.image.load("lago.png")
+        background = pygame.transform.scale(background, (largura, altura))
+
+
 
     # colisoes
     if (
@@ -306,6 +415,15 @@ while executando:
             and 0 <= proxima_posicao_y < altura - nova_altura
             and proxima_posicao_x % tamanho_bloco == 0
             and proxima_posicao_y % tamanho_bloco == 0
+            and not colisao_geocinese
+            and not colisao_pirotecnia
+            and not colisao_tormenta
+            and not colisao_vortex
+            and not colisao_peixe
+            and not colisao_panela
+            and not colisao_pedecabra
+            and not colisao_apito
+            and not colisao_botas
     ):
 
         if distancia_entre_personagem_e_blate < 100:  # Define a distância de proximidade
@@ -349,34 +467,6 @@ while executando:
             posicao_y = proxima_posicao_y
 
 
-    if fase_atual == 2:
-        background = pygame.image.load("refeitorio.png")
-        background = pygame.transform.scale(background, (largura, altura))
-
-    if fase_atual==3:
-        background = pygame.image.load("cozinha.png")
-        background = pygame.transform.scale(background, (largura, altura))
-
-    if fase_atual==4:
-        background = pygame.image.load("quadra.png")
-        background = pygame.transform.scale(background, (largura, altura))
-
-
-    if fase_atual == 5:
-        background = pygame.image.load("estacionamento.png")
-        background = pygame.transform.scale(background, (largura, altura))
-
-    if fase_atual == 6:
-        background = pygame.image.load("lixao.png")
-        background = pygame.transform.scale(background, (largura, altura))
-
-    if fase_atual == 7:
-        background = pygame.image.load("patio.png")
-        background = pygame.transform.scale(background, (largura, altura))
-
-    if fase_atual == 8:
-        background = pygame.image.load("lago.png")
-        background = pygame.transform.scale(background, (largura, altura))
 
 
 
@@ -401,6 +491,13 @@ while executando:
         tela.blit(slot_inventario, posicao_slot8)
         tela.blit(slot_inventario, posicao_slot9)
         tela.blit(slot_inventario, posicao_slot10)
+
+        for i, value in enumerate(slots):
+            tela.blit(value, posicao_item_slot[i])
+
+
+
+
 
     if fase_atual==3:
         if panela_visivel:
